bd6882e1ecddbf6a168b1d74aa0a80016e3a3fed
yud <phuocduydang010199@gmail.com>
2022-08-18 14:48:57 +0700

    ltvl.html

---
+++ source/btmang.js
@@ -0,0 +1,20 @@
+let names = Number(prompt("names"));
+let lists = new Array(names);
+/*lists[0] = name_1;
+lists[1] = name_2;
+lists[2] = name_3;
+lists[3] = name_4;
+lists[4] = name_5;
+lists[5] = name_6;
+lists[6] = name_7;
+console.log(lists[0]);
+console.log(lists[1]);
+console.log(lists[2]);
+console.log(lists[3]);
+console.log(lists[4]);
+console.log(lists[5]);
+console.log(lists[6]);*/
+
+for (i = 1; i < lists.length; i++) {
+  console.log(lists[i]);
+}
