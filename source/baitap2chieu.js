92347789713596214303534290404a0551e281fc
yud <phuocduydang010199@gmail.com>
2022-08-22 16:17:26 +0700

    songuyento.html

---
+++ source/baitap2chieu.js
@@ -0,0 +1,9 @@
+let maxtrix = [[numbers], [numbers], [numbers], [numbers]];
+let size = 5;
+let min = 10,
+  max = 50;
+let numbers = new Array(size);
+for (i = 1; i < array.length; i++) {
+  numbers[i] = Math.floor(Math.random() * (max - min + 1) + min);
+}
+document.write(numbers[i]);
